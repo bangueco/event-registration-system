c6f9eaeada4df03709f2089e8c6ce067316ae34b
ivandnd <121280104+ivandnd@users.noreply.github.com>
2024-01-03 21:06:30 +0800

    Add events database

---
+++ app/Models/Event.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'event_name',
+        'event_by',
+        'participants'
+    ];
+}
