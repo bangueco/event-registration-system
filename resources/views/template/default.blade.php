db21763a0d9e4936c074afeaa4ea94a78cca9750
ivandnd <121280104+ivandnd@users.noreply.github.com>
2024-01-04 23:38:24 +0800

    Add website template

--- resources/views/template/default.blade.php
+++ resources/views/template/default.blade.php
@@ -5,11 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 </head>
 
 <body>
     <header>
         <div class="icon">Event Registration System</div>
+        <nav class="links">
+            <a href="{{ route('home.page') }}">Events</a>
+            <a href="{{ route('manage.events') }}">Manage Events</a>
+            <a href="{{ route('logout.user') }}">Logout</a>
+        </nav>
     </header>
     <main>
         @yield('content')
